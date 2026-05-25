049c10ab1b7a436a8d77d6feadd529f64d3bc279
Michel Guerrero Obrador <micho.1990@gmail.com>
2026-05-25 14:23:23 -0400

    add yazi to repo

--- hypr/modules/windowsrules.lua
+++ hypr/modules/windowsrules.lua
@@ -31,4 +31,10 @@ hl.window_rule({
     no_focus = true,
 })
 
+hl.workspace_rule({ workspace = "1", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "2", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "3", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "4", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "5", monitor = "eDP-1", persistent = true })
+
 
