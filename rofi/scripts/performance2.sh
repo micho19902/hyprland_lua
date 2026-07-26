e4849ccd806dab7a463fdf4e4889efd014155e29
Michel Guerrero Obrador <micho.1990@gmail.com>
2026-07-25 23:42:56 -0400

    $1

--- rofi/scripts/performance2.sh
+++ rofi/scripts/performance2.sh
@@ -18,19 +18,16 @@ case ${perfil} in
 $performance)
   powerprofilesctl set performance
   brightnessctl set 96000
-  echo $perfil
   notify-send -u normal "Perfil de energía" "Cambiado a PERFORMANCE" -t 3000 -i /home/mich/.config/rofi/scripts/performance-svgrepo-com.svg
   ;;
 $powersaver)
   powerprofilesctl set power-saver
   brightnessctl set 10
-  echo $perfil
   notify-send -u normal "Perfil de energía" "Cambiado a POWER-SAVER" -t 3000 -i /home/mich/.config/rofi/scripts/performance-svgrepo-com.svg
   ;;
 $balanced)
   powerprofilesctl set balanced
   brightnessctl set 45000
-  echo $perfil
   notify-send -u normal "Perfil de energía" "Cambiado a BALANCED" -t 3000 -i /home/mich/.config/rofi/scripts/performance-svgrepo-com.svg
   ;;
 
